03aa6f734cd6cbc39339cd5c764444220fb653de
Johan Lindell <johan.lindell91@gmail.com>
2024-07-06 20:04:34 +0200

    Adds sidebar component

---
+++ src/app/my-grail/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BaseLayout from "../layouts/BaseLayout";
+
+const page = () => {
+  return (
+    <BaseLayout>
+      <main className='min-h-screen'>
+        <h2>My Grail</h2>
+      </main>
+    </BaseLayout>
+  );
+};
+
+export default page;
